426e80039a2b00243cb2a6a5fc00ec0d9fe4516c
Fernando Frazão <ffrazao@users.noreply.github.com>
2018-07-09 20:17:10 -0300

    Versão inicial

---
+++ src/main/java/br/com/frz/construtor/ConstrutorApplication.java
@@ -0,0 +1,12 @@
+package br.com.frz.construtor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstrutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstrutorApplication.class, args);
+	}
+}
