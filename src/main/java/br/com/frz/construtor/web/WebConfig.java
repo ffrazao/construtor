802a86ff7d6afa988caec944dcccccf371ca8e55
Fernando Frazão <ffrazao@users.noreply.github.com>
2018-07-11 22:29:32 -0300

    Ajustes para iniciar o layout

---
+++ src/main/java/br/com/frz/construtor/web/WebConfig.java
@@ -0,0 +1,17 @@
+package br.com.frz.construtor.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		//registry.addRedirectViewController("/", "index");
+	}
+}
---
+++ src/main/java/br/com/frz/construtor/web/controle/PrincipalCtrl.java
@@ -0,0 +1,9 @@
+package br.com.frz.construtor.web.controle;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PrincipalCtrl {
+	
+
+}
